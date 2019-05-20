92c0a0ce14d509746ea524965701d5833f10adc4
Jonathan Hensley <developer.jhensley@gmail.com>
2019-05-20 13:11:05 -0700

    Add remote_addr to request headers

--- app/views.py
+++ app/views.py
@@ -7,6 +7,8 @@ from . import app
 
 @app.route("/")
 def home():
-    return render_template("home.html", data=request.headers)
+    remote_addr = dict(remote_addr=request.remote_addr)
+    headers = {**remote_addr, **request.headers}
+    return render_template("home.html", data=headers)
 
 
